ff383d09bf2380e3d93fb90730265f429335e1e0
admin <admin@DESKTOP-BEI5PHI>
2022-11-15 15:59:49 -0800

    commit

---
+++ JavaDemo/src/practiceJavaDemo/Hello5.java
@@ -0,0 +1,16 @@
+package practiceJavaDemo;
+
+public class Hello5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		boolean var = false;
+		System.out.println(var);
+
+		char var1 = '\u00A7';
+		System.out.println(var1);
+
+
+	}
+
+}
