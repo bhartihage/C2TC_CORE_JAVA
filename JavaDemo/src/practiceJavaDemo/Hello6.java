8e06f5a1e03cc27221af4079c3c13bc6d650b012
admin <admin@DESKTOP-BEI5PHI>
2022-11-15 16:03:40 -0800

    COMMIT

---
+++ JavaDemo/src/practiceJavaDemo/Hello6.java
@@ -0,0 +1,15 @@
+package practiceJavaDemo;
+
+public class Hello6 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	       float f = 10.532f;
+	       long l = (long) f;
+	       System.out.println(l);
+
+
+	}
+
+}
